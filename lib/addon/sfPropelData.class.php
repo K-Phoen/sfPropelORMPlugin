a779fd4036912e9bdeef47c1f8da3eadfc03727a
Kévin Gomez Pinto <kevin_gomez@carpe-hora.com>
2012-03-02 11:44:10 +0100

    Fix: backported the PR #110 from the PropelBundle, which fixes the handling of array type in data loader

--- lib/addon/sfPropelData.class.php
+++ lib/addon/sfPropelData.class.php
@@ -147,7 +147,7 @@ class sfPropelData extends sfData
 
         foreach ($data as $name => $value)
         {
-          if (is_array($value) && 's' == substr($name, -1))
+          if (is_array($value) && 's' == substr($name, -1) && 'ARRAY' !== $tableMap->getColumn($name)->getType())
           {
             // many to many relationship
             $this->loadMany2Many($obj, substr($name, 0, -1), $value);
